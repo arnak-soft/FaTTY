d43d7668bb3c02eca8e083a7f1d8ae8ee92eef3c
arnak-soft <arnak.soft@gmail.com>
2026-09-17 14:32:26 +0300

    smth

--- build-mingw/generated/version_generated.hpp
+++ build-mingw/generated/version_generated.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-#define FATTY_VERSION_STRING "1.6.14-1-gc300c5f-dirty"
-#define FATTY_VERSION_TUPLE 1,6,14,1
+#define FATTY_VERSION_STRING "1.6.14-2-g77b7d70-dirty"
+#define FATTY_VERSION_TUPLE 1,6,14,2
